979ead1d570fc23aacb93613cb5658e8839ef1fd
ivanamarosevic <ivana.marosevic@levi9.com>
2024-12-22 19:59:51 +0100

    Create react vite project and configured tailwind

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+// import { useState } from "react";
+
+function App() {
+
+  return (
+    <>
+    <div className="h-screen w-full">
+      <div className="flex flex-col items-center justify-center w-full h-full bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-600">Welcome to My App</h1>
+      <p className="mt-4 text-lg text-blue-700 font-bold">
+        If you see this, Tailwind is working!
+      </p>
+    </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
